ca65fa484d20b6743e1186778806cc20c7669f1b
Alhassan Osman <oalhassan847@gmail.com>
2023-10-26 17:10:56 +0000

    Fix browser not opening

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import schedule
 import time
 import webbrowser
 
-def zoom_link(meeting_link):
-    webbrowser.open(meeting_link)
+zoom_link = "https://microverse.zoom.us/j/91369660757?pwd=MmRQZ2Ywa0NEb052ZlF5cXNka09Rdz09"
 
 def join_meeting():
     print('Joining')
-    zoom_link('google.com')
+    webbrowser.get().open(zoom_link)
     print('Joined')
 
-schedule.every().day.at("06:13").do(join_meeting)
+schedule.every().day.at("17:08").do(join_meeting)
 
 while True:
     schedule.run_pending()
